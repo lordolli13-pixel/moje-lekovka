d01ae519e4047980a8d580e6f9c3baa4a1670c6d
lordolli13-pixel <lord.olli13@gmail.com>
2026-02-09 19:32:19 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,31 +2,37 @@ const CACHE_NAME = 'lekovka-v6';
 const ASSETS = [
   './',
   './index.html',
-  './icon-512.jpg',
+  './icon-512.png',
   './manifest.json',
   'https://code.iconify.design/3/3.1.0/iconify.min.js',
   'https://cdn.tailwindcss.com'
 ];
 
-// Instalace - uloží soubory do cache
+// Instalace - uložení do paměti
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))
-  );
-});
-
-// Aktivace - vymaže staré verze cache
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => {
-      return Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)));
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
     })
   );
 });
 
-// Obsluha požadavků - nejdřív zkusí cache, pak síť
+// Aktivace - vyčištění staré mezipaměti
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
+  );
+});
+
+// Fetching - načítání souborů i bez internetu
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then(res => res || fetch(e.request))
+    caches.match(e.request).then((res) => {
+      return res || fetch(e.request);
+    })
   );
 });
